f7ab7dded06bbdd3ce2eed97ff7783aafbb921f1
Bernat Bonda <bondabear@gmail.com>
2019-03-28 17:43:16 +0100

    Profile config

--- src/main/java/org/github/saphyra/ipchecker/config/DevBeanConfig.java
+++ src/main/java/org/github/saphyra/ipchecker/config/DevBeanConfig.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.mail.javamail.JavaMailSender;
 
 @Configuration
-@Profile("!prod")
+@Profile("dev")
 public class DevBeanConfig {
     @Bean
     public JavaMailSender javaMailSender() {
